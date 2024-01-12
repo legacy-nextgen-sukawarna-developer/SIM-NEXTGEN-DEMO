987bde9b9b8cd4cbfac48c52415d3940cd66613e
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2024-01-12 17:17:42 +0700

    fixing role

--- app/Models/role.php
+++ app/Models/role.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class role extends Model
+class Role extends Model
 {
     use HasFactory;
 
@@ -14,6 +14,6 @@ class role extends Model
     // one to one relationship with the user
     public function user()
     {
-        return $this->hasOne(user::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
